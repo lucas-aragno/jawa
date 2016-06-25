b3197f07a69be2a403a5ed3848599fbbf96cd544
Lucas Aragno <lucas.aragno@moove-it.com>
2016-06-24 21:09:12 -0300

    Add Dashboard to Example

--- example/src/javascript/index.js
+++ example/src/javascript/index.js
@@ -3,21 +3,31 @@ import { render } from 'react-dom'
 
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { Route, Router } from 'react-router'
+import { Route, Router, push } from 'react-router'
 
-import { sessionReducer } from 'jawa'
+import { sessionReducer, SecureRoute } from 'jawa'
 
 import LogInPage from './components/LogInPage'
+import Dashboard from './components/Dashboard'
 
 const store = createStore(combineReducers({
   sessions: sessionReducer
 }))
 
+const secureFunction = () => {
+  if (3 > 2) {
+    console.log('see')
+  }
+}
+
 document.addEventListener('DOMContentLoaded', event => (
   render(
     <Provider store={store}>
       <Router>
-        <Route path="/" component={LogInPage} />
+        <Route path='/' component={LogInPage} />
+        <SecureRoute path='/' secureWith={secureFunction}>
+          <Route path='/dashboard' component={Dashboard} />
+        </SecureRoute>
       </Router>
     </Provider>, document.getElementById('root'))
 ))
